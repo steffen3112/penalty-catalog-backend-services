b67aa6ecf6bfbed9eccf2633737ef406a8aad5df
steffen3112 <schwarzs395@gmail.com>
2018-11-25 18:18:51 +0100

    Initialized JEE REST Backend Services Project for penalty-catalog

---
+++ README.md
@@ -0,0 +1,1 @@
+This project composes the RESTful Web-Services for the penalty-catalog project (https://github.com/steffen3112/penalty-catalog).
---
+++ src/main/java/com/penalty/catalog/services/JAXRSConfiguration.java
@@ -0,0 +1,10 @@
+package com.penalty.catalog.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+
+}
